c1427405bac602e0f93ec5432a371d4566c95c8b
Onuche Akor Idoko <gee_gog@yahoo.com>
2019-08-10 21:09:32 +0300

    Login Page Build Up
    small additions

---
+++ lib/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget{
+  @override
+  LoginState createState() => new LoginState();
+}
+
+class LoginState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold();
+  }
+
+}
