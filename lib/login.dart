5db0e5cbf7819a4fd3742722dc369604782fd4cf
Onuche Akor Idoko <gee_gog@yahoo.com>
2019-08-11 23:35:21 +0300

    Logo
    logo added

--- lib/login.dart
+++ lib/login.dart
@@ -9,6 +9,16 @@ class LoginState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('assets/logo.png'),
+      ),
+    );
+
     return new Scaffold();
   }
 
