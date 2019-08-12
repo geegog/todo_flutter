523f16836af8f174f794b1e1d7da5aa0952cce06
Onuche Akor Idoko <gee_gog@yahoo.com>
2019-08-12 03:23:01 +0300

    Login page
    text boxes added and button

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget{
+class LoginPage extends StatefulWidget {
   @override
   LoginState createState() => new LoginState();
 }
@@ -19,7 +19,77 @@ class LoginState extends State<LoginPage> {
       ),
     );
 
-    return new Scaffold();
-  }
+    final _formKey = GlobalKey<FormState>();
 
+    final form = Form(
+      key: _formKey,
+      child: Column(children: <Widget>[
+        TextFormField(
+          keyboardType: TextInputType.emailAddress,
+          autofocus: false,
+          decoration: InputDecoration(
+            hintText: 'Enter email',
+            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+          ),
+          validator: (value) {
+            if (value.isEmpty) {
+              return 'Please enter your email';
+            }
+            return null;
+          },
+        ),
+        SizedBox(height: 8.0),
+        TextFormField(
+          keyboardType: TextInputType.text,
+          autofocus: false,
+          decoration: InputDecoration(
+            hintText: 'Enter password',
+            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+          ),
+          validator: (value) {
+            if (value.isEmpty) {
+              return 'Please enter your password';
+            }
+            return null;
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: ButtonTheme(
+            minWidth: double.infinity,
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24),
+              ),
+              onPressed: () {
+                // Validate returns true if the form is valid, or false
+                // otherwise.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, display a Snackbar.
+
+                }
+              },
+              padding: EdgeInsets.all(12),
+              color: Colors.green,
+              child: Text('Log In', style: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ),
+      ]),
+    );
+
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[logo, SizedBox(height: 48.0), form],
+        ),
+      ),
+    );
+  }
 }
