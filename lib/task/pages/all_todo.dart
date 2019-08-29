1f77b70c17f72f958b613c4ebb66e14ce36f71f5
Onuche Akor Idoko <gee_gog@yahoo.com>
2019-08-29 23:41:14 +0300

    List data

--- lib/task/pages/all_todo.dart
+++ lib/task/pages/all_todo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_flutter/common/utils/api.dart';
 
 class AllTodoPage extends StatefulWidget {
   static const String tag = '/all-todos';
@@ -14,12 +15,81 @@ class AllTodoPage extends StatefulWidget {
 }
 
 class AllTodoState extends State<AllTodoPage> {
-  List todos;
+
+  String nextPage = "todo/all";
+  ScrollController _scrollController = new ScrollController();
+  bool isLoading = false;
+  List todos = new List();
 
   @override
-  void didUpdateWidget(AllTodoPage oldWidget) {
-    // TODO: implement didUpdateWidget
-    super.didUpdateWidget(oldWidget);
+  void initState() {
+    // TODO: implement initState
+    this._getTodos();
+    super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _getTodos();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  _getTodos() async {
+    if (!isLoading) {
+      setState(() {
+        isLoading = true;
+      });
+
+      final response = await APIUtil().fetch(nextPage);
+      List tempList = new List();
+      //nextPage = response.data['next'];
+      for (int i = 0; i < response['data'].length; i++) {
+        tempList.add(response['data'][i]);
+      }
+
+      setState(() {
+        isLoading = false;
+        todos.addAll(tempList);
+      });
+    }
+  }
+
+  Widget _buildProgressIndicator() {
+    return new Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Opacity(
+          opacity: isLoading ? 1.0 : 00,
+          child: new CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildList() {
+    return ListView.builder(
+      //+1 for progressbar
+      itemCount: todos.length + 1,
+      itemBuilder: (BuildContext context, int index) {
+        if (index == todos.length) {
+          return _buildProgressIndicator();
+        } else {
+          return new ListTile(
+            title: Text((todos[index]['name'])),
+            onTap: () {
+              print(todos[index]);
+            },
+          );
+        }
+      },
+      controller: _scrollController,
+    );
   }
 
   @override
