949db0cd6ce94eb2ce6b41f2e2807773440b2fba
Onuche Akor Idoko <gee_gog@yahoo.com>
2019-09-14 02:47:10 +0300

    Few updates

--- lib/common/domain/repository/user_repository.dart
+++ lib/common/domain/repository/user_repository.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:meta/meta.dart';
 import 'package:todo_flutter/common/services/auth.dart';
+import 'package:todo_flutter/common/services/service_locator.dart';
 import 'package:todo_flutter/common/utils/api.dart';
 
 class UserRepository {
@@ -19,24 +20,24 @@ class UserRepository {
   }
 
   void signOut() {
-    Auth().removeUser();
+    services.get<Auth>().removeUser();
     return;
   }
 
   Future<void> persistUser(String token) async {
-    await Auth().saveUser();
+    await services.get<Auth>().saveUser();
     return;
   }
 
   void persistToken(String token) {
-    return Auth().setToken(token);
+    return services.get<Auth>().setToken(token);
   }
 
   List<String> getUser() {
-    return Auth().getUser();
+    return services.get<Auth>().getUser();
   }
 
   bool hasValidToken() {
-    return !Auth().isTokenExpired();
+    return !services.get<Auth>().isTokenExpired();
   }
 }
